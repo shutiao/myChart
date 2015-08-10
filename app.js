a76232d541dc5fbe2e7226e8339307068c82a4ea
Haihua Xu <xuhaihua90@gmail.com>
2015-08-10 13:39:52 -0700

    add login view

---
+++ app.js
@@ -0,0 +1,32 @@
+// express is a nodejs library handling the routing and detailed web app
+// get a refernce to the module express.
+var express = require('express')
+    , app = express()
+    // consolidate is a set of wrapper for a number of template library for express   
+    , cons = require('consolidate')
+    , MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/test', function(err, db){
+	"use strict";
+	if(err) throw err;
+
+	// register the template engine, cons.swig based on file extension.
+	app.engine('html', cons.swig);
+	// view engine extension for express would be html by defalut.
+	app.set('view engine', 'html');
+	// set the directory for the application's view
+	app.set('views',process.cwd() + "/views");
+	app.set('port', (process.env.PORT || 8080));
+	// register a handle for the routes
+	app.get('/', function(req, res1){
+		//res.send("Hello, World!");
+		db.collection('helloworld').findOne({}, function(err, doc){
+			res1.render('hello', doc);
+		    });
+	    });
+	app.get('*', function(req1, res){
+		res.send("Page not found", 404);
+	    });
+	app.listen(app.get('port'));
+	console.log("Exress server started on port 8080");
+    });
